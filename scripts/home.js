def4db952c7ec8e09df305ee7215d0a1c80ab6f5
rleroudier <ext.osiatis.rleroudier@gmail.com>
2023-10-18 01:24:33 +0200

    refacto: home.js

--- scripts/home.js
+++ scripts/home.js
@@ -2,9 +2,7 @@ import { ProductService } from "../services/productService.js";
 const productService = new ProductService();
 productService.getProducts().then((products) => {
   const divProductsList = document.getElementById("productsList");
-  console.log("");
   for (const product of products) {
-    console.log(product);
     const div = document.createElement("div");
     div.classList.add("image");
     div.innerHTML = `<a href="info.html"><img src="${product.image}" class="livre"></a><p>${product.price}$<br>${product.title}</p>`;
